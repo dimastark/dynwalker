09dc8476d1c9022c90cd96f01d25f35073e4dd73
Дима Старков <dstarkdev@gmail.com>
2018-04-10 23:54:00 +0500

    Настоил docker-compose и пробросил url бекенда во фронт

--- dynwalker/frontend/src/components/app/App.js
+++ dynwalker/frontend/src/components/app/App.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react';
 export default class App extends PureComponent {
     render() {
         return (
-            <div/>
+            <div>{process.env.REACT_APP_SERVER_HOST}</div>
         );
     }
 }
