824114229b56172e4635cabb8a969a45f52ca817
Дима Старков <dstarkdev@gmail.com>
2018-04-11 13:27:53 +0500

    Починил docker для сервера

--- dynwalker/server/src/__main__.py
+++ dynwalker/server/src/__main__.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
 
-from sanic import Sanic
-from sanic.response import json
+from aoiklivereload import LiveReloader
 
-app = Sanic()
+from .app import app
 
-
-@app.route('/')
-async def test(request):
-    return json({'hello': 'world'})
-
-
-if __name__ == '__main__':
-    from aoiklivereload import LiveReloader
-    LiveReloader().start_watcher_thread()
-    app.run(host='0.0.0.0', port=8000)
+LiveReloader().start_watcher_thread()
+app.run(host='0.0.0.0', port=8000)
